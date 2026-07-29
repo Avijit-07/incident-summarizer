4addf4c12819cc453ac5175e7f967039c4776455
avijit_maiti <avijit.haldia@gmail.com>
2026-07-30 01:21:04 +0530

    chore: moving model & url config in AnthropicConfig class

--- src/main/java/dev/avijit/incidentsummarizer/IncidentSummarizerApplication.java
+++ src/main/java/dev/avijit/incidentsummarizer/IncidentSummarizerApplication.java
@@ -1,9 +1,13 @@
 package dev.avijit.incidentsummarizer;
 
+import dev.avijit.incidentsummarizer.config.AnthropicConfig;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AnthropicConfig.class)
 public class IncidentSummarizerApplication {
 
     public static void main(String[] args) {
