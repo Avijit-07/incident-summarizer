e9055990c4cc128199beb51b832b4cf40d4bbcd2
avijit_maiti <avijit.haldia@gmail.com>
2026-07-30 01:21:04 +0530

    chore: moving model & url config in AnthropicConfig class

--- src/main/java/dev/avijit/incidentsummarizer/config/AnthropicConfig.java
+++ src/main/java/dev/avijit/incidentsummarizer/config/AnthropicConfig.java
@@ -1,10 +1,12 @@
 package dev.avijit.incidentsummarizer.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 
 @ConfigurationProperties(prefix = "anthropic")
 public record AnthropicConfig(
+        String url,
         String apiKey,
-        String model,
+        @DefaultValue("claude-sonnet-4-5") String model,
         int maxTokens
 ) {}
